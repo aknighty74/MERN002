c9fe640fd90d237ea4261dbe30a2bf753bd91c6d
Mendoza Steven <syem74@outlook.es>
2023-06-28 15:34:44 -0500

    first repo upload

---
+++ server/index.js
@@ -0,0 +1,56 @@
+//app de chat - chatappX
+
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/auth");
+const messageRoutes = require("./routes/messages");
+const app = express();
+const socket = require("socket.io");
+//const helmet = require("helmet");
+require("dotenv").config();
+
+//import helmet from "helmet";
+app.use(cors());
+app.use(express.json());
+//app.use(helmet());
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Conexion a la DB exitosa");
+  })
+  .catch((err) => {
+    console.log("No se pudo establecer conexion a la base de datos");
+  });
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+const server = app.listen(process.env.PORT, () =>
+  console.log(`Proceso de servidor iniciado en puerto ${process.env.PORT}`)
+);
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true,
+  },
+});
+
+global.onlineUsers = new Map();
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.msg);
+    }
+  });
+});
