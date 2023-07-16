9db0f943d732bcb5a8e437db9fa92b0883aeb961
Mendoza Steven <syem74@outlook.es>
2023-07-16 03:14:42 -0500

    testeodebcrypt

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -37,3 +37,47 @@ module.exports.addMessage = async (req, res, next) => {
     next(ex);
   }
 };
+
+/*
+const Messages = require("../models/messageModel");
+const bcrypt = require("bcrypt");
+
+module.exports.getMessages = async (req, res, next) => {
+  try {
+    const { from, to } = req.body;
+
+    const messages = await Messages.find({
+      users: {
+        $all: [from, to],
+      },
+    }).sort({ updatedAt: 1 });
+
+    const projectedMessages = messages.map((msg) => {
+      return {
+        fromSelf: msg.sender.toString() === from,
+        message: msg.message.text,
+      };
+    });
+    res.json(projectedMessages);
+  } catch (ex) {
+    next(ex);
+  }
+};
+
+module.exports.addMessage = async (req, res, next) => {
+  try {
+    const { from, to, message } = req.body;
+    const hashedMessage = await bcrypt.hash(message, 10);
+    const data = await Messages.create({
+      message: { text: hashedMessage },
+      users: [from, to],
+      sender: from,
+    });
+
+    if (data) return res.json({ msg: "Mensaje agregado correctamente" });
+    else return res.json({ msg: "Error al logear mensaje en la DB" });
+  } catch (ex) {
+    next(ex);
+  }
+};
+*/
